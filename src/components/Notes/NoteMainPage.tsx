84b481fa0c4b8cf811d380f280196bd674817c95
오유진 <omh232323@gmail.com>
2023-04-18 10:21:34 +0900

    Suspense test

--- src/components/Notes/NoteMainPage.tsx
+++ src/components/Notes/NoteMainPage.tsx
@@ -1,23 +1,26 @@
 import { colorThemeAtom } from "@/utils/atoms";
 import { returnColors } from "@/utils/utilFn";
 import { Center, Heading, VStack, Box, Divider } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
-import NoteCategorySelector from "./NoteCategorySelector";
 import dynamic from "next/dynamic";
 import LoadingGrid from "./LoadingGrid";
 
-interface INotesMainPageProps {
-  category: string;
-  size: number;
-}
+const NoteCategorySelector = dynamic(() => import("./NoteCategorySelector"), {
+  ssr: false,
+});
 
 const NoteGridPage = dynamic(() => import("./NoteGridPage"), {
   loading: () => <LoadingGrid />,
   ssr: false,
 });
 
-export default function NotesMainPage({ category, size }: INotesMainPageProps) {
+interface INotesMainPageProps {
+  category: string;
+  size: number;
+}
+
+function NotesMainPageContent({ category, size }: INotesMainPageProps) {
   const colorTheme = useRecoilValue(colorThemeAtom);
   const [lightColor, setLightColor] = useState("");
 
@@ -27,19 +30,48 @@ export default function NotesMainPage({ category, size }: INotesMainPageProps) {
   }, [colorTheme]);
 
   return (
-    <>
-      <VStack h="auto" justifyContent={"flex-start"} position={"relative"}>
-        <Box
-          w="100vw"
-          h="40vh"
-          position={"absolute"}
-          zIndex={-1}
-          backgroundRepeat="no-repeat"
-          backgroundAttachment={"fixed"}
-          backgroundSize="cover"
-          backgroundPosition={"center center"}
-          backgroundImage="/assets/imgs/main/miles.jpeg"
-        />
+    <VStack h="auto" justifyContent={"flex-start"} position={"relative"}>
+      <Box
+        w="100vw"
+        h="40vh"
+        position={"absolute"}
+        zIndex={-1}
+        backgroundRepeat="no-repeat"
+        backgroundAttachment={"fixed"}
+        backgroundSize="cover"
+        backgroundPosition={"center center"}
+        backgroundImage="/assets/imgs/main/miles.jpeg"
+      />
+      <Box
+        w="100vw"
+        h="40vh"
+        position={"absolute"}
+        zIndex={1}
+        top={-2}
+        background={
+          "repeating-linear-gradient(0deg,#0e0d0e 25%,#0e0d0e 50%, #171819 50%,  #171819 75%)"
+        }
+        backgroundSize="10px 10px"
+        opacity={0.3}
+      />
+      <Center minH={"40vh"} color="white" zIndex={2}>
+        <Heading textShadow={`3px 3px ${lightColor}`} fontSize={"7xl"}>
+          Notes
+        </Heading>
+      </Center>
+      <VStack position={"relative"}>
+        <NoteCategorySelector category={category} />
+        <NoteGridPage category={category} size={size} />
+        <Divider py={3} />
+      </VStack>
+    </VStack>
+  );
+}
+
+export default function NotesMainPage({ category, size }: INotesMainPageProps) {
+  return (
+    <React.Suspense
+      fallback={
         <Box
           w="100vw"
           h="40vh"
@@ -52,17 +84,9 @@ export default function NotesMainPage({ category, size }: INotesMainPageProps) {
           backgroundSize="10px 10px"
           opacity={0.3}
         />
-        <Center minH={"40vh"} color="white" zIndex={2}>
-          <Heading textShadow={`3px 3px ${lightColor}`} fontSize={"7xl"}>
-            Notes
-          </Heading>
-        </Center>
-        <VStack position={"relative"}>
-          <NoteCategorySelector category={category} />
-          <NoteGridPage category={category} size={size} />
-          <Divider py={3} />
-        </VStack>
-      </VStack>
-    </>
+      }
+    >
+      <NotesMainPageContent category={category} size={size} />
+    </React.Suspense>
   );
 }
