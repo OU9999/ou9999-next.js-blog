968abcbe0444226a316d1f339f25b4289bc03516
오유진 <omh232323@gmail.com>
2023-04-20 11:43:52 +0900

    sitemap test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
 
+const Sitemap = require("sitemap");
 const removeImports = require("next-remove-imports")();
+
+const generateSitemap = async () => {
+  const sitemap = new Sitemap.SitemapStream({
+    hostname: "https://ou9999-next-js-blog.vercel.app/",
+  });
+
+  sitemap.write({ url: "/", priority: 1.0, changefreq: "daily" });
+  sitemap.end();
+  return sitemap.toString();
+};
 
 const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
+  },
+  exportPathMap: async () => {
+    const sitemap = await generateSitemap();
+    return {
+      "/sitemap.xml": { page: "/sitemap.xml", query: { sitemap } },
+    };
   },
 };
 
